0886895da333ffa2b588ecfa59af4a73f1268af6
Shukevich Alexandr <shukevich.alexandr@gmail.com>
2025-10-23 00:23:05 +0300

    homeWork-commit3

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -34,7 +34,7 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-       <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
@@ -47,13 +47,13 @@ export const HW3 = () => {
       <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
       <ol id={'hw03-tasks'}>
-       {texts.map((el, index) => {
-         return (
-           <li key={index} id={`hw03-task-${index}`}>
-             {el}
-           </li>
-         );
-       })}
+        {texts.map((el, index) => {
+          return (
+            <li key={index} id={`hw03-task-${index}`}>
+              {el}
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
