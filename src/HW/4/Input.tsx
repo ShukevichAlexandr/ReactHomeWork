0a7686809dd97d1682735a34eb34f248594facae
Shukevich Alexandr <shukevich.alexandr@gmail.com>
2025-10-29 22:43:26 +0300

    homeWork1-commit4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,14 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+  	setCurrentText: React.Dispatch<React.SetStateAction<string>>; // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
-export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+export const Input = ({ currentText, setCurrentText }: InputPropsType) => {
+    const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentText(event.target.value); // Устанавливаем новое состояние input
+    };
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+	<input id={'hw04-input'} type="text" value={currentText} onChange={onChangeHandler} />
+	);
 };
